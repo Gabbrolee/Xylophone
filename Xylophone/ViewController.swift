88e89636fe465a4fb927361e7793e1579c4c6f2b
Gabbrolee <akrine2christophine@gmail.com>
2021-09-13 00:53:10 +0100

    Xylophone Task

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -1,21 +1,34 @@
-//
-//  ViewController.swift
-//  Xylophone
-//
-//  Created by Angela Yu on 28/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+      playSound(sender.currentTitle!)
+      print(sender.backgroundColor)
+      //print(sender.title(for: .normal)!)
+      print(sender.currentTitle)
+      //print(sender.titleLabel?.text)
+      
+    }
+  
+  func playSound(_ soundName: String) {
+    let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+    player = try! AVAudioPlayer(contentsOf: url!)
     
-
+    player.play()
+  }
 }
+
+
+//these code snippet perform the same task above
+//print(sender.title(for: .normal)!)
+//print(sender.currentTitle)
+//print(sender.titleLabel?.text)
 
